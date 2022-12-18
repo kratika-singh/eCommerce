8309e24d94f907ad30761c1ab3a629fb61a6744e
kratika-singh <105146516+kratika-singh@users.noreply.github.com>
2022-12-18 13:05:31 +0530

    dfghjkl

--- script.js
+++ script.js
@@ -1,14 +1,18 @@
+/* Shop Page */
+
 var mainImg = document.getElementById("mainImg");
 var smallImg = document.getElementsByClassName("small-img");
-smallImg[0].onclick = function(){
+smallImg[0].onclick = function () {
     mainImg.src = smallImg[0].src;
 }
-smallImg[1].onclick = function(){
+smallImg[1].onclick = function () {
     mainImg.src = smallImg[1].src;
 }
-smallImg[2].onclick = function(){
+smallImg[2].onclick = function () {
     mainImg.src = smallImg[2].src;
 }
-smallImg[3].onclick = function(){
+smallImg[3].onclick = function () {
     mainImg.src = smallImg[3].src;
 }
+
+
